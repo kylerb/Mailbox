94ecb78d2bd245cc0bbbf1a5ac966f9aadfb1291
Your Name HereKyler <bluekylerblue@gmail.com>
2016-10-26 00:09:23 -0700

    Four colors animating in on pan gesture early version

--- Mailbox/MailboxViewController.swift
+++ Mailbox/MailboxViewController.swift
@@ -16,56 +16,86 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet weak var messageView: UIView!
     
+    @IBOutlet weak var messageBackgroundView: UIView!
+    
+    
     var messageOriginalCenter: CGPoint!
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    scrollView.delegate = self
-    scrollView.contentSize = contentView.frame.size
-
-    
+        scrollView.delegate = self
+        scrollView.contentSize = contentView.frame.size
+        
+        messageOriginalCenter = messageView.center
         
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     @IBAction func didPanMessge(_ sender: UIPanGestureRecognizer) {
         
-        let location = sender.location(in: view)
-        let velocity = sender.velocity(in: view)
         let translation = sender.translation(in: view)
         
         if sender.state == .began {
-            print("Gesture began")
+            
             messageOriginalCenter = messageView.center
+            
         } else if sender.state == .changed {
-            print("Gesture is changing")
+            
             messageView.center = CGPoint(x: messageOriginalCenter.x + translation.x, y: messageOriginalCenter.y)
-        } else if sender.state == .ended {
-            print("Gesture ended")
+            
+            if messageView.center.x < 100 && messageView.center.x >= -60 {
+                
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.messageBackgroundView.backgroundColor = UIColor.yellow
+                })
+                
+            }   else if messageView.center.x < -60  {
+                
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.messageBackgroundView.backgroundColor = UIColor.orange
+                })
+                
+            }   else if messageView.center.x > 260 && messageView.center.x < 325 {
+                
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.messageBackgroundView.backgroundColor = UIColor.green
+                })
+                
+            }   else if messageView.center.x > 380  {
+                
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.messageBackgroundView.backgroundColor = UIColor.red
+                })
+                
+                
+                
+                
+            } else if sender.state == .ended {
+                print("Gesture ended")
+            }
+            
+            
+            
         }
         
-        
-        
     }
-    
-    
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
